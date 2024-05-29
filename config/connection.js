c7f569c2b738e9f91b48b69bdc89880b3e8c94cb
JacobToton <Jake_Toton@live.com>
2024-05-29 12:16:18 -0600

    Completed First Attempt @ Tag-Routes / De-Bugged .env file errors.
    First attempt completion of tag-routes:
    - GET ALL
    - GET SINGLE
    - POST NEW
    - PUT / UPDATE
    - DELETE
    
    Debugged .env file errors.
    Error: must be string.  All fields were coming back as undefined.
    
    Added .env.text file and test.js to debug.  Updated packages.  Re-installed npm.  Updated connection.js , index.js to accomodate the issues.

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,24 @@
-require('dotenv').config();
+require("dotenv").config();
+const Sequelize = require("sequelize");
 
-const Sequelize = require('sequelize');
+console.log("DB_NAME:", process.env.DB_NAME);
+console.log("DB_USER:", process.env.DB_USER);
+console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
+console.log("DB_PASSWORD type:", typeof process.env.DB_PASSWORD);
 
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: "localhost",
+        dialect: "postgres",
+        dialectOptions: {
+          decimalNumbers: true,
+        },
+      }
+    );
 
 module.exports = sequelize;
---
+++ test.js
@@ -0,0 +1,2 @@
+require("dotenv").config({ path: ".env.test" });
+console.log("TEST_VARIABLE:", process.env.TEST_VARIABLE);
